5d1763575471af75125827bafef85bcce1ec4cb5
joshorion7 <joshorion7@gmail.com>
2018-04-19 07:45:54 -0400

    create first project

---
+++ CateringCostApp/index.js
@@ -0,0 +1,13 @@
+/*var express = require('express');
+var mysql = require('mysql');
+var app = express();
+var https = require('https');
+
+var connection = mysql.createConnection ({
+// properties...
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: '' //database name within ''
+})
+*/
